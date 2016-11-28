64a47178bb78769eb2569d6cefde1df1ee544d49
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-11-28 10:00:04 +0900

    use NEON on ARM

--- benchmark-distance/main.cpp
+++ benchmark-distance/main.cpp
@@ -7,13 +7,22 @@
 #include <stdexcept>
 #include <limits>
 #include <random>
+
+#if defined _M_X64 || (defined _M_IX86_FP && _M_IX86_FP >= 2)
 #include <intrin.h>
+#define USE_SSE 1
+#elif ((defined WIN32 || defined _WIN32) && defined(_M_ARM)) || defined(__ARM_NEON__) || (defined (__ARM_NEON) && defined(__aarch64__))
+#include <arm_neon.h>
+#define USE_SSE 0
+#else
+#error // use either SSE or NEON
+#endif
 
 // Parameter D (dimension of a vector with uchar elements)
 // has to be set in multiples of ALIGN (i.e., 16).
 // This benchmark treats the length of a binary vector as 8D.
 
-const int ALIGN=16; // alignment step for SSE
+const int ALIGN=16; // alignment step for SIMD
 const unsigned D=128; // dimension of a vector with uchar elements
 const unsigned N=1024*1024*4; // # of dictionary vectors
 
@@ -46,7 +55,8 @@ inline int dist_l2(unsigned char* p,unsigned char* q)
 		result+=(q[d]-p[d])*(q[d]-p[d]);
 	return result;
 }
-inline int dist_l2_sse(unsigned char* p,unsigned char* q)
+#if USE_SSE 
+inline int dist_l2_simd(unsigned char* p,unsigned char* q)
 {
 	__m128i t=_mm_setzero_si128();
 	for(unsigned d=0;d<D;d+=ALIGN)
@@ -70,6 +80,31 @@ inline int dist_l2_sse(unsigned char* p,unsigned char* q)
 	}
 	return t.m128i_i32[0]+t.m128i_i32[1]+t.m128i_i32[2]+t.m128i_i32[3];
 }
+#else
+inline int dist_l2_simd(unsigned char* p,unsigned char* q)
+{
+	uint32x4_t t = vdupq_n_u32((unsigned)0);
+	for(unsigned d=0;d<D;d+=ALIGN)
+	{
+		uint8x16_t pm = vld1q_u8(p+d);
+		uint8x16_t qm = vld1q_u8(q+d);
+        uint16x8_t plo = vmovl_u8(vget_low_u8(pm));
+        uint16x8_t qlo = vmovl_u8(vget_low_u8(qm));
+        uint16x8_t sublo = vabdq_u16(plo, qlo);
+        sublo = vmulq_u16(sublo, sublo);
+        t = vaddq_u32(vmovl_u16(vget_low_u16(sublo)), t);
+        t = vaddq_u32(vmovl_u16(vget_high_u16(sublo)), t);
+
+        plo = vmovl_u8(vget_high_u8(pm));
+        qlo = vmovl_u8(vget_high_u8(qm));
+        sublo = vabdq_u16(plo, qlo);
+        sublo = vmulq_u16(sublo, sublo);
+        t = vaddq_u32(vmovl_u16(vget_low_u16(sublo)), t);
+        t = vaddq_u32(vmovl_u16(vget_high_u16(sublo)), t);
+    }
+    return vgetq_lane_u32(t, 0) + vgetq_lane_u32(t, 1) + vgetq_lane_u32(t, 2) + vgetq_lane_u32(t, 3);
+}
+#endif
 
 inline int dist_l1(unsigned char* p,unsigned char* q)
 {
@@ -78,7 +113,8 @@ inline int dist_l1(unsigned char* p,unsigned char* q)
 		result+=std::abs(q[d]-p[d]);
 	return result;
 }
-inline int dist_l1_sse(unsigned char* p,unsigned char* q)
+#if USE_SSE
+inline int dist_l1_simd(unsigned char* p,unsigned char* q)
 {
 	__m128i t=_mm_setzero_si128();
 	for(unsigned d=0;d<D;d+=ALIGN)
@@ -92,6 +128,17 @@ inline int dist_l1_sse(unsigned char* p,unsigned char* q)
 	}
 	return int(t.m128i_i64[0]+t.m128i_i64[1]);
 }
+#else
+inline int dist_l1_simd(unsigned char* p,unsigned char* q)
+{
+    uint32x4_t t = vdupq_n_u32(0);
+	for(unsigned d=0;d<D;d+=ALIGN)
+	{
+        t = vaddq_u32(t, vpaddlq_u16(vpaddlq_u8(vabdq_u8(vld1q_u8(p+d), vld1q_u8(q+d)))));
+	}
+    return vgetq_lane_u32(t, 0) + vgetq_lane_u32(t, 1) + vgetq_lane_u32(t, 2) + vgetq_lane_u32(t, 3);
+}
+#endif
 
 //bit count by the D&C algorithm
 inline int popcount32(unsigned int x)
@@ -128,6 +175,8 @@ inline int dist_hamming32(unsigned char* p,unsigned char* q)
 	}
 	return int(result);
 }
+
+#if USE_SSE
 inline int dist_hamming32_sse(unsigned char* p,unsigned char* q)
 {
 	int result=0;
@@ -143,6 +192,7 @@ inline int dist_hamming32_sse(unsigned char* p,unsigned char* q)
 	}
 	return int(result);
 }
+#endif
 
 inline int dist_hamming64(unsigned char* p,unsigned char* q)
 {
@@ -156,7 +206,8 @@ inline int dist_hamming64(unsigned char* p,unsigned char* q)
 	}
 	return int(result);
 }
-inline int dist_hamming64_sse(unsigned char* p,unsigned char* q)
+#if USE_SSE
+inline int dist_hamming64_simd(unsigned char* p,unsigned char* q)
 {
 	long long result=0;
 	unsigned long long* p64=reinterpret_cast<unsigned long long*>(p);
@@ -164,11 +215,24 @@ inline int dist_hamming64_sse(unsigned char* p,unsigned char* q)
 	for(unsigned d=0;d<D;d+=ALIGN)
 	{
 		// this is probably faster than using _mm_xor_si128().
-		result+=_mm_popcnt_u64((*p64++)^(*q64++));
-		result+=_mm_popcnt_u64((*p64++)^(*q64++));
+		result+ = _mm_popcnt_u64((*p64++)^(*q64++));
+		result+ = _mm_popcnt_u64((*p64++)^(*q64++));
 	}
 	return int(result);
 }
+#else
+inline int dist_hamming64_simd(unsigned char* p,unsigned char* q)
+{
+	uint16x4_t result = vdup_n_u16(0);
+	for(unsigned d=0;d<D;d+=ALIGN)
+	{
+		uint8x16_t t = vcntq_u8(veorq_u8(vld1q_u8(p+d), vld1q_u8(q+d)));
+        uint8x8_t t0 = vpadd_u8(vget_low_u8(t), vget_high_u8(t)); // 16 -> 8
+        result = vadd_u16(result, vpaddl_u8(t0)); // 8 -> 4
+	}
+    return int(vget_lane_u16(result, 0) + vget_lane_u16(result, 1) + vget_lane_u16(result, 2) + vget_lane_u16(result, 3));
+}
+#endif
 
 inline std::pair<int,int> search(unsigned char* dict,unsigned char* query)
 {
@@ -196,10 +260,10 @@ inline std::pair<int,int> search_sse(unsigned char* dict,unsigned char* query)
 	for(unsigned n=0;n<N;++n)
 	{
 		// uncomment one of them as you like!
-//		int d=dist_l2_sse(&dict[n*D],query);
-//		int d=dist_l1_sse(&dict[n*D],query);
-//		int d=dist_hamming32_sse(&dict[n*D],query);
-		int d=dist_hamming64_sse(&dict[n*D],query);
+//		int d=dist_l2_simd(&dict[n*D],query);
+//		int d=dist_l1_simd(&dict[n*D],query);
+//		int d=dist_hamming32_simd(&dict[n*D],query);
+		int d=dist_hamming64_simd(&dict[n*D],query);
 		if(best_d<d)
 			continue;
 		best_n=n;
